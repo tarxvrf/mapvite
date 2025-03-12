9daeea2eb921b5193cf9e7a3f5c7a9d6ac5bfea9
tarxvrf <tarxvrf@gmail.com>
2025-03-12 23:25:27 +0700

    perbaikan code

--- src/Map.tsx
+++ src/Map.tsx
@@ -10,10 +10,10 @@ const CustomMap = () => {
     if (mapRef.current && !mapInstance.current) {
       // Inisialisasi peta hanya sekali
       mapInstance.current = L.map(mapRef.current,{
-        center: [51.505, 1], // Posisi peta (untuk memberi pusat peta sementara)
+        center: [50, 1], // Posisi peta (untuk memberi pusat peta sementara)
         zoom: 5,
-        zoomControl: false, // Menonaktifkan kontrol zoom
-        attributionControl: false ,// Menonaktifkan kontrol atribusi peta
+        zoomControl: true, // Menonaktifkan kontrol zoom
+        attributionControl: false ,// Menonaktifkan logo ukraine
         maxZoom: 13, // Jika perlu batasi zoom maksimal
       minZoom: 1 // Jika perlu batasi zoom minimal
       })
@@ -23,16 +23,25 @@ const CustomMap = () => {
       // Menambahkan Gambar sebagai Image Overlay
       const imageUrl = '/depok.jpg'; // Path relatif gambar yang ada di dalam folder public
      
-      L.imageOverlay(imageUrl,L.latLngBounds([[10, -20.0], [60.0, 30.0]]) ).addTo(mapInstance.current); // Menambahkan gambar ke peta
+      L.imageOverlay(imageUrl,L.latLngBounds([[40, -20.0], [60.0, 30.0]]) ).addTo(mapInstance.current); // Menambahkan gambar ke peta
 
       // Menambahkan Marker di atas gambar
        // Koordinat untuk marker (sesuaikan dengan gambar kamu)
-      const marker = L.marker([50,0]).addTo(mapInstance.current);
-      marker.bindPopup( `<style>h1{color:red;}</style><h1>SOld out</h1>
-        <p>ini harganya 100juta</p>
+      const marker = L.marker([45,4]).addTo(mapInstance.current);
+      marker.bindPopup( `<style>h1{color:red;}</style><h1>Kecamatan</h1>
+        <p>Cilodong</p>
         
         `);
-    
+        const marker2 = L.marker([47,10]).addTo(mapInstance.current);
+      marker2.bindPopup( `<style>h1{color:red;}</style><h1>Kecamatan</h1>
+        <p>Tapos</p>
+        
+        `);
+      const marker3= L.marker([45.6,-3]).addTo(mapInstance.current);
+      marker3.bindPopup( `<style>h1{color:red;}</style><h1>Kecamatan</h1>
+        <p>Cipayung</p>
+        
+        `);
     }
   }, []); // Hanya jalankan sekali saat komponen pertama kali dirender
 
