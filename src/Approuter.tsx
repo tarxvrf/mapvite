2b7db246ef6c79d4df5b84bff651c6d4be60983d
tarxvrf <tarxvrf@gmail.com>
2025-03-13 17:25:10 +0700

    perbaikan code

--- src/Approuter.tsx
+++ src/Approuter.tsx
@@ -1,22 +1,26 @@
 // src/App.tsx
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-//import Report from './Dashboard/Report';
-//import Ringkasan from './Dashboard/Ringkasan';
-//import Stokbarang from './Dashboard/Stokbarang';
-//import Sales from './Dashboard/Sales';
+import Report from './Dashboard/Report';
+import Ringkasan from './Dashboard/Ringkasan';
+import Stokbarang from './Dashboard/Stokbarang';
+//import Deliver from './Dashboard/Deliver';
+import Sales from './Dashboard/Sales';
 import CustomMap from './Map';
-import Home from './Home';
 
 const AppRouter = () => {
  
-  return (    
+  return (
+    <div>
     <Router>
       <Routes>  
         <Route path="/" element={<CustomMap/>} />
-        <Route path="/home" element={<Home/>} />
+        <Route path="/dashboard/report" element={<Report />} />
+        <Route path="/dashboard/stokbarang" element={<Stokbarang />} />       
+        <Route path="/dashboard/sales" element={<Sales />} />
+        <Route path="/dashboard/" element={<Ringkasan />} />
       </Routes>
     </Router>
- 
+ </div>
   );
 };
 
--- src/main.tsx
+++ src/main.tsx
@@ -1,23 +1,17 @@
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+
 import { createRoot } from 'react-dom/client'
 import './styles/index.css'
 //import App from './App.tsx'
-import 'leaflet/dist/leaflet.css';
+import 'leaflet/dist/leaflet.css';  
 //import Dashboard from './Dashboard.tsx';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-//import AppRouter from './Approuter.tsx';
-import CustomMap from './Map.tsx';
-import Home from './Home.tsx';
+import AppRouter from './Approuter.tsx';
 
 createRoot(document.getElementById('root')!).render(
-
     <QueryClientProvider client={new QueryClient}>
-         <Router>
-      <Routes>  
-        <Route path="/" element={<CustomMap/>} />
-        <Route path="/home" element={<Home/>} />
-      </Routes>
-    </Router>
-    </QueryClientProvider>
+    <div className='min-h-screen bg-black p-4'>
+     <AppRouter/>
+    </div>   
+     </QueryClientProvider>
 
 )
