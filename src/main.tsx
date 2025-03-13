6a792cb9d5245eccfc0b08c310a594f05bdd2971
tarxvrf <tarxvrf@gmail.com>
2025-03-13 17:13:13 +0700

    perbaikan code

--- src/main.tsx
+++ src/main.tsx
@@ -1,18 +1,23 @@
-
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import { createRoot } from 'react-dom/client'
 import './styles/index.css'
 //import App from './App.tsx'
 import 'leaflet/dist/leaflet.css';
 //import Dashboard from './Dashboard.tsx';
-//import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-import AppRouter from './Approuter.tsx';
-import { StrictMode } from 'react';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+//import AppRouter from './Approuter.tsx';
+import CustomMap from './Map.tsx';
+import Home from './Home.tsx';
 
 createRoot(document.getElementById('root')!).render(
-    <StrictMode>
-        
-          
-                <AppRouter />
-            
-    </StrictMode>
+
+    <QueryClientProvider client={new QueryClient}>
+         <Router>
+      <Routes>  
+        <Route path="/" element={<CustomMap/>} />
+        <Route path="/home" element={<Home/>} />
+      </Routes>
+    </Router>
+    </QueryClientProvider>
+
 )
